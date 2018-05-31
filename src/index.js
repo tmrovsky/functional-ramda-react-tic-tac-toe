a698d38858cf8334c0cb27a37e8781d216b27f30
Tomasz Ciunel <ciunel@desmart.com>
2018-05-31 21:04:07 +0200

    Delete boilerplate create react app code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<h1>Let's become Functional!</h1>, document.getElementById('root'))
+registerServiceWorker()
