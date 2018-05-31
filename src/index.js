19370a941f18c4d4bc9b15b042ba14eb6a3a6ad0
Tomasz Ciunel <ciunel@desmart.com>
2018-05-31 21:59:04 +0200

    Normalize styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
+import 'assets/scss/index.scss'
 
 ReactDOM.render(<h1>Let's become Functional!</h1>, document.getElementById('root'))
 registerServiceWorker()
